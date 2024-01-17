c22c3ebc460faaecd515c227737c5234ae36707a
Santiago Melo <santiagocano15cr7@gmail.com>
2024-01-16 18:04:15 -0600

    component donde aprendemos useState

--- projects/01-primeros-pasos/src/components/TwiiterFollowCart.jsx
+++ projects/01-primeros-pasos/src/components/TwiiterFollowCart.jsx
@@ -1,5 +1,33 @@
+import { useState } from "react"
 
-export function TwitterCart ({children, userName, name, isFollowing}) {
+// Si quieres que una prop, tenga un valor por defecto, cuando no utilizes la prop
+// es (nombre de la prop) = (cualquier tipo de valor)
+
+// generalmente las props que sirve para inicializar estado esque el nombre de la prop que tu utilices
+// la iniciales con initial(nombre de tu prop, recuerda el camelcase jijijij)
+export function TwitterCart ({children, userName , name, initialIsFollowing }) {
+
+    // useState, forma de acutalizar la ui 
+    // la primera posicion es el valor
+    // el segundo es la forma de actualizar este valor
+    const [isFollowing, setIsFollowing] = useState(initialIsFollowing)
+
+    // forma de manejar el text, si es follow, damos el text de siguiendo pero si hacemos lo contrario
+    // estariamos dejando de seguir, por ende el texto seria el de seguir
+    const text = isFollowing ? "Siguiendo" : "Seguir"
+
+    // esto es exactamente lo mismo de arriba solo que con css
+    const buttonCss = isFollowing ? "tw-followCard-button is-following" : "tw-followCard-button"
+
+    // hacer click, damos la funcion
+    const handleClick = () => {
+
+        // decimos que el segundo estado, osea el que actualiza la ui,
+        // damos que el primer estado que por defecto es false
+        // lo damos con "!" osea que no existe, asi hacemos que cambie el texto y se regrese al valor inicial
+        setIsFollowing(!isFollowing)
+    
+    }
 
     return (
 
@@ -26,7 +54,18 @@ export function TwitterCart ({children, userName, name, isFollowing}) {
 
             <aside>
 
-                <button className="tw-followCard-button">Seguir</button>
+                {/* Damos el onClick y le damos la funcion de arriba */}
+                <button 
+                
+                    onClick={handleClick} 
+                    className={buttonCss}
+                    
+                >
+                    <span className="tw-followCard-text">{text}</span>
+
+                    <span className="tw-followCard-stopFollow">Dejar de seguir</span>
+                    
+                </button>
 
             </aside>
 
