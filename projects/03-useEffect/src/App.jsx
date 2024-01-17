d33dd1731c55cac458bc7d3ff613bc4f36d4c824
Santiago Melo <santiagocano15cr7@gmail.com>
2024-01-17 02:51:38 -0600

    Pointer Move

--- projects/03-useEffect/src/App.jsx
+++ projects/03-useEffect/src/App.jsx
@@ -1,10 +1,32 @@
+import { useState } from "react"
 import "./App.css"
+import { PointerMove } from "./components/PointerMove"
 
 function App() {
 
+  const [mounted, setMounted] = useState(true)
+
   return (
+
+    <main style={{display: "flex", flexDirection: "column", gap: "8px"}}>
+
+      {mounted && <PointerMove></PointerMove>}
+
+      <button 
+      
+        onClick={() => setMounted(!mounted)}
+        
+      > 
+
+        {mounted ? "Desmontar el seguir puntero, si no es de tu agrado" 
+         
+         : "Recuperar el seguir puntero, por si cambiaste de opinion"
+
+        }
+      
+      </button>
     
-    <h3>Proyecto 3</h3>
+    </main>
     
   )
   
