714852e237c0d9481b2a016b29b78dbc3ad39f90
Santiago Melo <santiagocano15cr7@gmail.com>
2024-01-21 13:10:43 -0600

    cambiando el bg del boton

--- projects/08-talwind/src/app/components/Button.jsx
+++ projects/08-talwind/src/app/components/Button.jsx
@@ -6,7 +6,7 @@ export default function Button() {
         
         <button 
         
-            className="bg-gradient-to-r from-teal-400 to-teal-600 rounded-md px-4 py-2 w-48 mx-auto
+            className="bg-gradient-to-r from-teal-400 to-teal-900 rounded-md px-4 py-2 w-48 mx-auto
             shadow-lg text-white text-xl mt-3"
         
         > Este es mi boton</button>
