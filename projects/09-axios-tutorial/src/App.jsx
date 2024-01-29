e7ec35f940517841cfacb78d36e6c8017c1cf6b6
Santiago Melo <santiagocano15cr7@gmail.com>
2024-01-29 16:19:21 -0600

    Utilizando axios en react

--- projects/09-axios-tutorial/src/App.jsx
+++ projects/09-axios-tutorial/src/App.jsx
@@ -1,10 +1,57 @@
-
+import axios from "axios"
+import { useEffect, useState } from "react"
+import { API_KEY, LANGUAGE } from "../config"
 
 export function App() {
 
-  return (
+  const [list, setList] = useState([])
+
+  const mostrandoPelis = async() => {
+
+    try {
+
+      const response = await axios.get("https://api.themoviedb.org/3/movie/popular", 
       
-    
+      { params: { api_key: API_KEY, language: LANGUAGE} })
+
+      setList(response.data.results)
+      
+    } catch (error) {
+
+      console.log(error)
+      
+    }
+
+  }
+
+  useEffect(() => { mostrandoPelis() }, [])
+  
+  return (
+
+    <div className="contenedor">
+
+        <ul className="movies">
+
+          {list.map((text) => (
+
+            <li key={text.id}>
+
+              <h1 className="text-sm text-red-500">{text.original_title}</h1>
+
+              <span>{text.overview}</span>
+
+              <span>{text.popularity}</span>
+
+              <img src={text.backdrop_path}></img>
+
+            </li>
+  
+          ))}
+
+        </ul>
+
+        </div>
+
   )
 
 }
